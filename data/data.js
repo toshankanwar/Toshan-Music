06b83bbb536aeaca61279faa336afb7c86ad2340
toshan kanwar <toshankanwar03@gmail.com>
2026-03-27 22:22:57 +0530

    cgl song

--- data/data.js
+++ data/data.js
@@ -25,6 +25,19 @@ export const musicData = [
       coverUrl: "https://cdn.jsdelivr.net/gh/toshankanwar/cdn-toshan@main/covers/tu-hi-mera-ghar.webp",
       color: "#1DB954",
     },
+    {
+      id: 3,
+      slug: "cgl-motivation",
+      title: "Cgl Motivation",
+      artist: "Toshan kanwar",
+      album: "Cgl",
+      duration: "5:14",
+      year: 2026,
+      genre: "Motivation",
+      audioUrl: "https://static-assets.toshankanwar.in/music/cgl-motivation.mp3",
+      coverUrl: "https://static-assets.toshankanwar.in/music/cgl-motivation.png",
+      color: "#1DB954",
+    },
     //} Add more tracks...
   ];
   
